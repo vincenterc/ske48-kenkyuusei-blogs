203aab069ef461b5808d687d31d56cf28c48b4d6
vincenterr <chung.menghsun@gmail.com>
2018-12-12 14:50:26 +0800

    Add members reducer and initial state of members in redux store

--- src/Store.js
+++ src/Store.js
@@ -2,6 +2,21 @@ import { createStore } from 'redux'
 import reducers from './reducers'
 import generations from '../content/generations'
 
+const members = generations.reduce((gacc, g) => {
+  const generation = g.members.reduce(
+    (macc, m) => ({
+      ...macc,
+      [m.id]: m,
+    }),
+    {}
+  )
+
+  return {
+    ...gacc,
+    ...generation,
+  }
+}, {})
+
 const posts = generations.reduce((gacc, g) => {
   const generation = g.members.reduce(
     (macc, m) => ({
@@ -28,6 +43,7 @@ const posts = generations.reduce((gacc, g) => {
 
 const initialState = {
   generations,
+  members,
   posts,
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import generations from './Generations'
+import members from './Members'
 import posts from './Posts'
 
 export default combineReducers({
   generations,
+  members,
   posts,
 })
