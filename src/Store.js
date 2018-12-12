a69e34ecfced9a7bab7af758573a2bf69cd7503c
vincenterr <chung.menghsun@gmail.com>
2018-12-12 11:14:40 +0800

    Add posts reducer and initial state of posts in redux store

--- src/Store.js
+++ src/Store.js
@@ -2,8 +2,33 @@ import { createStore } from 'redux'
 import reducers from './reducers'
 import generations from '../content/generations'
 
+const posts = generations.reduce((gacc, g) => {
+  const generation = g.members.reduce(
+    (macc, m) => ({
+      ...macc,
+      [m.id]: {
+        list: m.posts,
+        map: m.posts.reduce(
+          (pacc, p) => ({
+            ...pacc,
+            [p.id]: p,
+          }),
+          {}
+        ),
+      },
+    }),
+    {}
+  )
+
+  return {
+    ...gacc,
+    ...generation,
+  }
+}, {})
+
 const initialState = {
   generations,
+  posts,
 }
 
 const store = createStore(reducers, initialState)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
-import generations from './Members'
+import generations from './Generations'
+import posts from './Posts'
 
 export default combineReducers({
   generations,
+  posts,
 })
