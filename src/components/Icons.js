33833354a798341a335b6b08a9c3cfbcb88d551a
vincenterr <chung.menghsun@gmail.com>
2019-03-20 23:16:49 +0800

    Add Icons

---
+++ src/components/Icons.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MdAdd, MdRemove } from 'react-icons/md'
+
+const Plus = props => <MdAdd {...props} />
+
+const Minus = props => <MdRemove {...props} />
+
+export { Plus, Minus }
