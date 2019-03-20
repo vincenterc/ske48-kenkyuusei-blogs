40b84c78de699c3f75616dafbb3e7544f3bab4f7
vincenterr <chung.menghsun@gmail.com>
2019-03-20 23:18:08 +0800

    Implement expanding/unexpanding of members of generations in side bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
+import * as Icons from './Icons'
 
 class Sidebar extends React.Component {
   render() {
@@ -32,25 +33,43 @@ const Wrapper = styled.div`
   }
 
   & > .content {
-    padding: 10px 15px;
+    padding: 10px 15px 10px 0;
     background: #fff9ed;
   }
 `
 
 class Generation extends React.Component {
+  state = {
+    expanded: true,
+  }
+
   render() {
     let { extraCss, generation } = this.props
+    let { expanded } = this.state
 
     return (
       <Generation.Wrapper extraCss={extraCss}>
-        <Generation.Title>{`${generation.title.toUpperCase()}`}</Generation.Title>
+        <Generation.Header>
+          <Generation.Expander
+            onClick={() => this.setState({ expanded: !expanded })}
+          >
+            {expanded ? (
+              <Icons.Minus size="14px" />
+            ) : (
+              <Icons.Plus size="14px" />
+            )}
+          </Generation.Expander>
+          <Generation.Title>{`${generation.title.toUpperCase()}`}</Generation.Title>
+        </Generation.Header>
 
-        {generation.members.map(m => (
-          <Generation.Member key={m.identity} to={`/${m.identity}`}>
-            {m.name}
-            {/*`(${m.posts.length})`*/}
-          </Generation.Member>
-        ))}
+        {expanded
+          ? generation.members.map(m => (
+              <Generation.Member key={m.identity} to={`/${m.identity}`}>
+                {m.name}
+                {/*`(${m.posts.length})`*/}
+              </Generation.Member>
+            ))
+          : ''}
       </Generation.Wrapper>
     )
   }
@@ -59,11 +78,22 @@ class Generation extends React.Component {
     ${props => props.extraCss}
   `
 
+  static Header = styled.div`
+    display: flex;
+    align-items: center;
+  `
+
+  static Expander = styled.div`
+    color: #e57e16;
+    cursor: pointer;
+  `
+
   static Title = styled.h4`
     color: #e57e16;
   `
 
   static Member = styled(Link)`
+    padding-left: 16px;
     display: block;
     color: #e57e16;
     font-size: 14px;
